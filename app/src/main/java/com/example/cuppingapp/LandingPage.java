d4f6a43a2908ef4c8f995ba8dc730d1a5f4570f7
Gisele732 <annelise.mornard@gmail.com>
2024-08-11 20:41:51 +1200

    Add toolbar and main menu to landing page

--- app/src/main/java/com/example/cuppingapp/LandingPage.java
+++ app/src/main/java/com/example/cuppingapp/LandingPage.java
@@ -2,11 +2,17 @@ package com.example.cuppingapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,6 +26,9 @@ public class LandingPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_landing_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setIcon(R.drawable.logo_white_sml);
 
         buttonLogout = findViewById(R.id.buttonLogout);
 
@@ -37,4 +46,26 @@ public class LandingPage extends AppCompatActivity {
             return insets;
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("LandingPage", "onCreateOptionsMenu called");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.view:
+                Toast.makeText(getApplicationContext(), "Start View Activity", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.add:
+                Toast.makeText(getApplicationContext(), "Start Add Activity", Toast.LENGTH_SHORT).show();
+                return (true);
+        }
+        return (super.onOptionsItemSelected(item));
+    }
+
 }
